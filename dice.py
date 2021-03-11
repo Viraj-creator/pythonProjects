b1e9af2876a0f756057fa204e1f57311c888d29c
Viraj Patil <69743653+Viraj-creator@users.noreply.github.com>
2021-03-11 09:15:02 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 23 09:38:21 2020
+
+@author: raj patil
+"""
+""" DICE STIMULATOR """
+
+import random
+num= random.randint(1,6)
+print("DICE STIMULATOR")
+
+
+if num == 1:
+    print("---------")
+    print("|       |")
+    print("|   0   |")
+    print("|       |")
+    print("---------")
+    
+if num == 2:
+    print("---------")
+    print("|       |")
+    print("|  0 0  |")
+    print("|       |")
+    print("---------")
+
+if num == 3:
+    print("---------")
+    print("|   0   |")
+    print("|   0   |")
+    print("|   0   |")
+    print("---------")
+
+if num == 4:
+    print("---------")
+    print("| 0   0 |")
+    print("|       |")
+    print("| 0   0 |")
+    print("---------")
+if num == 5:
+    print("---------")
+    print("|0     0|")
+    print("|   0   |")
+    print("|0     0|")
+    print("---------")
+if num == 6:
+    print("---------")
+    print("|0     0|")
+    print("|0     0|")
+    print("|0     0|")
+    print("---------")    
+    
+    
+print(num)
+
+
